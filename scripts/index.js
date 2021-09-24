4681e1c4d65f279c2613b7383b1e9b8d7f467a03
Igor Grechuhin <imehanik@yandex.ru>
2021-09-25 00:55:08 +0500

    добавить функционал js/ footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,35 @@
-let popup = document.querySelector(".popup");
-let editButton = document.querySelector(".edit-button");
-let closeButton = document.querySelector(".popup__close-button");
-console.log(editButton);
+const popup = document.querySelector(".popup");
+const editButton = document.querySelector(".edit-button");
+const profileName = document.querySelector(".profile__name");
+const profileProfession = document.querySelector(".profile__profession");
+const form = popup.querySelector(".input-form");
+const closeButton = popup.querySelector(".popup__close-button");
+const inputName = popup.querySelector(".input-form__item_user_name");
+const inputJob = popup.querySelector(".input-form__item_user_job");
 
 function togglePopup() {
   popup.classList.toggle("popup_is-opened");
 }
 
-editButton.addEventListener("click", togglePopup);
+function updateInputForm() {
+  inputName.value = profileName.textContent;
+  inputJob.value = profileProfession.textContent;
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  let name = inputName.value;
+  let job = inputJob.value;
+  profileName.textContent = name;
+  profileProfession.textContent = job;
+  togglePopup();
+}
+
+editButton.addEventListener("click", function () {
+  togglePopup();
+  updateInputForm();
+});
+
 closeButton.addEventListener("click", togglePopup);
+
+form.addEventListener("submit", formSubmitHandler);
